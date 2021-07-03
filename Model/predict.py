aa6998e12a11d4ff1fb7689c27b914e99b01d107
Biswajeet Sah <Biswajeet.Sah@go-mmt.com>
2021-07-03 23:12:28 +0530

    Pushing Model

---
+++ Model/predict.py
@@ -0,0 +1,40 @@
+from keras.models import load_model
+import os 
+import numpy as np
+import cv2 
+import pickle
+
+WIDTH = 224
+HEIGHT =224
+
+
+def predict( WIDTH = WIDTH, HEIGHT = HEIGHT):
+
+	cwd = os.getcwd()
+	model_path = os.path.join(cwd, 'Model','model.h5')
+	if not os.path.exists(model_path):
+		raise FileNotFoundError('Model not found')
+
+	model = load_model(model_path)
+	
+	data = cv2.imread(os.path.join(cwd , 'test_image.png'), 0 )
+	img = cv2.resize(data , (WIDTH,HEIGHT))
+	x_test = [img]
+	x_test = np.asarray(x_test)
+	x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)
+	
+	prediction = model.predict(x_test)
+	data = []
+
+	labels = ['covid','pneumonia', 'normal']
+	for pred in prediction:
+		temp = {}
+		value_list = list(pred)
+		for ind , val in enumerate(value_list):
+			temp[labels[ind]] = float(val)
+
+		max_index = value_list.index(max(value_list))
+		report = labels[max_index]
+		data.append({"health_condition": report,"values":temp})
+
+	return data[0]
---
+++ README.md
@@ -0,0 +1,18 @@
+## COVID PNEUMONIA NORMAL Predicton 
+
+### Download the Model from [here](https://drive.google.com/file/d/1QlLlTZxyzvwJ_Z7-mXQXDQFosX8aRrXB/view?usp=sharing) & unzip it and move it to Model folder 
+
+### Install requirements 
+```
+pip install -r requirements 
+
+```
+
+### Change directory to code base and run 
+```
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload 
+
+```
+
+
+### Open url in the browser - http://0.0.0.0:8000/docs 
